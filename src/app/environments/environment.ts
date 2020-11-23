3c41fd7535ecc1cd6bd3276487d7861ee68b0dee
Lucas Camargo <lucasvsj7@gmail.com>
2020-11-23 00:40:13 -0300

    Upload Project

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyALilXgT2iDOLfU3pNFNYX6rOrQF4lXyrU',
+        authDomain: 'barbershop-ab4b9.firebaseapp.com',
+        databaseURL: 'https://barbershop-ab4b9.firebaseio.com',
+        projectId: 'barbershop-ab4b9',
+        storageBucket: 'barbershop-ab4b9.appspot.com',
+        messagingSenderId: '480650656323',
+        appId: '1:480650656323:web:e8c36810bc6859234972f6'
+    }
+};
+
